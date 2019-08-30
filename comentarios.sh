3aeee41e860a7b597960426999f8e9239c284b84
Tarcisio Câmara <41845115+tattudc@users.noreply.github.com>
2019-08-30 20:36:34 -0200

    Comentarios
    Comentarios em Shell

---
+++ comentarios.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#Esse e o primeiro programa para mostrar conceitos basicos
+#
+
+#Usamos a primeira variavel para ler um nome
+NOME="Tarcisio"
+echo "ola ${NOME}"
+
+#Usamos segunda variavel para atribuir um valor
+NOME="Tatu"
+echo "ola ${NOME}"
+
+#Terceira variavel para ler um nome e printar
+echo "Entre com seu nome"
+read MEU_NOME
+echo "ola ${MEU_NOME}"
