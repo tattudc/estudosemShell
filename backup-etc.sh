20472af5b0e380c25248c604adbe93248782ddac
Tarcisio Câmara <41845115+tattudc@users.noreply.github.com>
2019-09-07 21:24:03 -0200

    Update backup

--- backup-etc.sh
+++ backup-etc.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
+
+# Código criado por Tarcísio Dantas Câmara
+
 #Setar data
 DATA=$(date +%y-%m-%d_%H"h"%M"m"%S"s")
 
-#Destino e origem
-DIR_ORIG="../../etc"
-DIR_DEST="../../tmp/backup"
+#Origem etc - arquivos .sh
+DIR_ORIG="!(*.sh) /etc"
 
-#arquivo de backup criado
+#arquivo de backup criado com variavel Data 
 BKP_NAME="backup-etc-${DATA}.tar.gz"
 
-#Criar pasta
-mkdir -p ../../tmp/backup
+#Perguntar a pasta 
+echo "Para iniciar o backup, digite a pasta de destino"
+read DIR_DEST
+#Condicional para testar se existe o arquivo
 
+if[ -e ${DIR_DEST}];
+then
+#Se dir destino existir então cria o arquivo de backup
 tar -czpf ${DIR_DEST}/${BKP_NAME} ${DIR_ORIG}
+echo "Backup criado com sucesso!!!"
 
-#Mensagem
-echo "Seu backup foi realizado com sucesso"
-echo "Diretorio ${DIR_ORIG}"
-echo "Destino: ${DIR_DEST}${BKP_NAME}"
+else
+#Se não cria pasta e ai cria o backup
+mkdir -p ${DIR_DEST}
+echo "Diretorio ${DIR_DEST} criado com sucesso!!!"
+tar -czpf ${DIR_DEST}/${BKP_NAME} ${DIR_ORIG}
+echo "Backup criado com sucesso!!!"
+fi
+
 exit 0
