c7d8ee528249c505d9f915090a2ed1302d67e430
Tarcisio Câmara <41845115+tattudc@users.noreply.github.com>
2019-09-18 09:17:37 -0200

    Criando grupos
    Inserindo criação de grupos no for

--- sistemaempresa.sh
+++ sistemaempresa.sh
@@ -24,15 +24,16 @@ USUARIOS=("josuejunior" "alessandra" "beniciocamara" "edivaniasales" "janiasilva
 mkdir -p ${PASTAEMP[0]}
 cd ${PASTAEMP[0]}
 
-#Criando pastas setores e usuarios
+#Criando pastas e grupos de setores
 for ((i=1; i<=6; i++))
 do
-mkdir -p ${PASTAEMP[i]}
+addgroup ${PASTAEMP[$i]}
+mkdir -p ${PASTAEMP[$i]}
 done
 cd ${PASTAEMP[6]}
 
 #Criando pastas para os usuarios
 for ((i=0; i<=14; i++))
 do
-mkdir -p ${USUARIOS[i]}
+mkdir -p ${USUARIOS[$i]}
 done
