ec437fac8040b77e146e6b7cf2a478ad2e91fd91
Tarcisio Câmara <41845115+tattudc@users.noreply.github.com>
2019-09-18 11:34:44 -0300

    Grupos, usuarios
    falta acesso

--- sistemaempresa.sh
+++ sistemaempresa.sh
@@ -6,34 +6,51 @@ if [ -e /srv ];
 then
 #Se dir srv existir
 echo "Pasta srv ja existe"
-cd /srv
 
 else
 #Se não cria pasta
 mkdir -p /srv
-cd /srv
 echo "Pasta criada com sucesso!!!"
 fi
+cd /srv
 
 #Variaveis em lista para criar as pastas e usuarios
-PASTASEMP=("TarcisioTI" "setorRH" "setorEstoque" "setorMedico" "setorFinanceiro" "setorAdm" "usuarios")
+PASTASEMP=("TarcisioTI" "setorrh" "setorestoque" "setormedico" "setorfinanceiro" "setoradm" "usuarios")
 USUARIOS=("josuejunior" "alessandra" "beniciocamara" "edivaniasales" "janiasilva" "magdagurgel" "joanamagno" "tiagoazevedo" "julianogermano" "conceicao" "claudiogodeiro" "waleska" "maratatiane" "morganamagno" "joaobatista")
 
 #Criando pastas
 #Criando pasta empresa e entrando nela
-mkdir -p ${PASTAEMP[0]}
-cd ${PASTAEMP[0]}
+mkdir -p ${PASTASEMP[0]}
+cd ${PASTASEMP[0]}
 
 #Criando pastas e grupos de setores
 for ((i=1; i<=6; i++))
 do
-addgroup ${PASTAEMP[$i]}
-mkdir -p ${PASTAEMP[$i]}
+addgroup ${PASTASEMP[$i]}
+mkdir -p ${PASTASEMP[$i]}
 done
-cd ${PASTAEMP[6]}
+cd ${PASTASEMP[6]}
 
 #Criando pastas para os usuarios
 for ((i=0; i<=14; i++))
 do
+adduser ${USUARIOS[$i]}
 mkdir -p ${USUARIOS[$i]}
 done
+
+#Adicionando usuarios aos grupos
+gpasswd -a ${USUARIOS[0]} ${PASTASEMP[1]}
+gpasswd -a ${USUARIOS[1]} ${PASTASEMP[1]}
+gpasswd -a ${USUARIOS[2]} ${PASTASEMP[1]}
+gpasswd -a ${USUARIOS[3]} ${PASTASEMP[2]}
+gpasswd -a ${USUARIOS[4]} ${PASTASEMP[2]}
+gpasswd -a ${USUARIOS[5]} ${PASTASEMP[2]}
+gpasswd -a ${USUARIOS[6]} ${PASTASEMP[3]}
+gpasswd -a ${USUARIOS[7]} ${PASTASEMP[3]}
+gpasswd -a ${USUARIOS[8]} ${PASTASEMP[3]}
+gpasswd -a ${USUARIOS[9]} ${PASTASEMP[4]}
+gpasswd -a ${USUARIOS[10]} ${PASTASEMP[4]}
+gpasswd -a ${USUARIOS[11]} ${PASTASEMP[4]}
+gpasswd -a ${USUARIOS[12]} ${PASTASEMP[5]}
+gpasswd -a ${USUARIOS[13]} ${PASTASEMP[5]}
+gpasswd -a ${USUARIOS[14]} ${PASTASEMP[5]}
