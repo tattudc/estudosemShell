e74bbf6d64d948029194e6c170685b992e2467f5
Tarcisio Câmara <41845115+tattudc@users.noreply.github.com>
2019-09-18 09:12:40 -0200

    Criação com for
    Resumindo o código

--- sistemaempresa.sh
+++ sistemaempresa.sh
@@ -25,27 +25,14 @@ mkdir -p ${PASTAEMP[0]}
 cd ${PASTAEMP[0]}
 
 #Criando pastas setores e usuarios
-mkdir -p ${PASTAEMP[1]}
-mkdir -p ${PASTAEMP[2]}
-mkdir -p ${PASTAEMP[3]}
-mkdir -p ${PASTAEMP[4]}
-mkdir -p ${PASTAEMP[5]}
-mkdir -p ${PASTAEMP[6]}
+for ((i=1; i<=6; i++))
+do
+mkdir -p ${PASTAEMP[i]}
+done
 cd ${PASTAEMP[6]}
 
 #Criando pastas para os usuarios
-mkdir -p ${USUARIOS[0]}
-mkdir -p ${USUARIOS[1]}
-mkdir -p ${USUARIOS[2]}
-mkdir -p ${USUARIOS[3]}
-mkdir -p ${USUARIOS[4]}
-mkdir -p ${USUARIOS[5]}
-mkdir -p ${USUARIOS[6]}
-mkdir -p ${USUARIOS[7]}
-mkdir -p ${USUARIOS[8]}
-mkdir -p ${USUARIOS[9]}
-mkdir -p ${USUARIOS[10]}
-mkdir -p ${USUARIOS[11]}
-mkdir -p ${USUARIOS[12]}
-mkdir -p ${USUARIOS[13]}
-mkdir -p ${USUARIOS[14]}
+for ((i=0; i<=14; i++))
+do
+mkdir -p ${USUARIOS[i]}
+done
