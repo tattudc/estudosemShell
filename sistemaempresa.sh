b6fd862dd507b66568916d157ad901d038f52f85
Tarcisio Câmara <41845115+tattudc@users.noreply.github.com>
2019-09-18 22:21:48 -0300

    Finalizando ativ 6

--- sistemaempresa.sh
+++ sistemaempresa.sh
@@ -54,3 +54,37 @@ gpasswd -a ${USUARIOS[11]} ${PASTASEMP[4]}
 gpasswd -a ${USUARIOS[12]} ${PASTASEMP[5]}
 gpasswd -a ${USUARIOS[13]} ${PASTASEMP[5]}
 gpasswd -a ${USUARIOS[14]} ${PASTASEMP[5]}
+
+#Atribuindo grupo de setor em cada pasta respectiva
+cd /srv/${PASTASEMP[0]}
+for ((i=1; i<=5; i++))
+do
+chgrp ${PASTASEMP[$i]} ${PASTASEMP[$i]}
+done
+
+#Atribuindo usuario em cada pasta respectiva
+cd /srv/${PASTASEMP[0]}/${PASTASEMP[6]}
+for ((i=0; i<=14; i++))
+do
+sudo chown ${USUARIOS[$i]} /srv/${PASTASEMP[0]}/${PASTASEMP[6]}/${USUARIOS[$i]}
+done
+
+#Permissões
+#Permissão pasta TarcisioTI(empresa não pode escrever)
+cd /srv
+sudo chmod 555 /srv/${PASTASEMP[0]} -R
+
+#Permissão da pasta de setores
+cd /srv/${PASTASEMP[0]}
+sudo chmod 575 ${PASTASEMP[1]} -R
+sudo chmod 575 ${PASTASEMP[2]} -R
+sudo chmod 575 ${PASTASEMP[3]} -R
+sudo chmod 575 ${PASTASEMP[4]} -R
+sudo chmod 575 ${PASTASEMP[5]} -R
+
+#Permissão de cada usuario
+cd /srv/${PASTASEMP[0]}/${PASTASEMP[6]}
+for ((i=0; i<=14; i++))
+do
+sudo chmod 700 ${USUARIOS[$i]} -R
+done
